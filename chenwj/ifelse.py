b27ce2f230de0513943cb53a8523fd5cc9907c58
chenwj <731028571@qq.com>
2017-05-10 11:47:08 +0800

    trial [link](trail.md)

---
+++ chenwj/ifelse.py
@@ -0,0 +1,7 @@
+age = 20
+if age > 100:
+	print("haha")
+elif age > 60:
+	print("hehe")
+else:
+	print("ok")
---
+++ chenwj/loop.py
@@ -0,0 +1,11 @@
+arr = ["arr1", "foo", "bar"]
+for i in arr:
+	print(i)
+print(range(5))
+print(list(range(5)))
+print("--------")
+limit = 5
+while limit > 0:
+	print("haha")
+	limit -= 1
+print("-------")
