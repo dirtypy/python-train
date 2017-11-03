259d0b12846e99d177f28febe447f5a6b946f609
Sean Yan <huaianyanxu@163.com>
2017-11-03 18:53:25 +0800

    Grammar programmer of python annonation&interpreter .

---
+++ cn/yanxml/hello/Annonation.py
@@ -0,0 +1,28 @@
+#coding=UTF-8
+
+#Anonation Type 1: #
+
+print ("HELLO")
+# print ("HELLO")
+
+
+#Anonation Type 2: '''
+
+'''
+print ("HELLO2")
+print ("HELLO2")
+print ("HELLO2")
+'''
+print ("HELLO2")
+
+
+#Anonation Type 2: """
+
+"""
+print ("HELLO3")
+print ("HELLO3")
+print ("HELLO3")
+"""
+
+print ("HELLO3")
+
---
+++ cn/yanxml/hello/Interpreter.py
@@ -0,0 +1,37 @@
+#coding=UTF-8
+
+# localhost:python-train Sean$ which python
+# /usr/local/bin/python
+# localhost:python-train Sean$ which python3
+# /usr/local/bin/python3
+
+
+# interactive programming(交互式编程)
+
+# localhost:python-train Sean$ python3
+# Python 3.6.2 (default, Jul 17 2017, 16:44:47)
+# [GCC 4.2.1 Compatible Apple LLVM 8.0.0 (clang-800.0.42.1)] on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+
+# TEST 1
+#     >>> print("hello")
+# hello
+
+# TEST 2
+# >>> flag=True
+# >>> if flag:
+#     ...  print("Falg=True")
+# ... else:
+# ...  print("Flag!=True")
+# ...
+# Falg=True
+# >>>
+
+
+# python3 interpreter.py
+
+
+#! /usr/bin/env python3
+# $ chmod +x interpreter.py
+# ./interpreter.py
+# result: Hello, Python!
